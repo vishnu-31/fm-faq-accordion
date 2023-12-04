aa296a995f68ab584214801cb944318cd5280bf1
vishnu-31 <vishnuvicky3636@gmail.com>
2023-12-04 17:23:20 +0530

    added animation and set 1st item default to be open

--- src/AccordionItem.tsx
+++ src/AccordionItem.tsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
 
 export type inputType ={
+    id:number;
     question: string;
     answer: string;
+    openItem: React.ComponentState;
+    setOpenItem: React.Dispatch<number>;
   }
 export const AccordionItem = (props: inputType) => {
+    const [isOpen, setIsOpen] = useState(props.id==0 ? true : false);
 
+    const toggleFn = () => {
+      if( props.id == props.openItem){
+        setIsOpen(!isOpen)
+      }
+      else{
+        props.setOpenItem(props.id)
+        setIsOpen(true)
+      }
+    }
 
-    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="flex-col py-2">
-        <div className="flex justify-between font-semibold text-lg my-3">
-          <div className='w-[14em] lg:w-[28em] text-md  active:text-purple-600 cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
+        <div className="flex justify-between font-semibold text-lg my-2">
+          <div className='w-[14em] md:w-[28em] text-md  active:text-purple-600 cursor-pointer' onClick={toggleFn}>
             {props.question}
           </div>
-          <button onClick={() => setIsOpen(!isOpen)}><img className='w-8 h-8' src={isOpen? "/images/icon-minus.svg" :"/images/icon-plus.svg"} alt="Minus icon"/></button>
+          <button onClick={toggleFn}><img className='w-8 h-8' src={(props.openItem==props.id&&isOpen)? "/images/icon-minus.svg" :"/images/icon-plus.svg"} alt="Minus icon"/></button>
         </div>
-        {isOpen && 
-        <div className="text-slate-400 py-2 ">
-          {props.answer}
-
+        
+        <div className={`grid text-slate-400 py-2 transition-all duration-300 ease-in-out overflow-hidden ${
+          ((props.openItem==props.id)&&isOpen)? "grid-rows-[1fr] opacit-100": "grid-rows-[0fr] opacity-0"
+        }`}>
+          <div className="overflow-hidden">
+            {props.answer}
+          </div>
         </div>
-        }
+  
       </div>
     )
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,52 @@
 
 
-import { AccordionItem, inputType } from './AccordionItem'
+import { useState } from 'react'
+import { AccordionItem, } from './AccordionItem'
 import './App.css'
 
 
 
-const prop: inputType[] = [{
+const prop = [{
+  id:0,
   question: "What is Frontend Mentor, and How will it help me?",
-  answer: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
+  answer: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
   },
   {
+    id:1,
     question:"Is Frontend Mentor free?",
-    answer: "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
+    answer: "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+
   },
   {  
+    id:2,
     question:"Can I use Frontend Mentor projects in my portfolio?",
-    answer:"Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
+    answer:"Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+
   },
   {
+    id:3,
     question: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
-    answer: "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
+    answer: "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
   }
 
 ]
 
 function App() {
-  
+  const [openItem, setOpenItem] = useState(0);
   return (
-    <main className='flex flex-col w-[23em] lg:w-[36em] bg-white rounded-lg p-7'>
-      <div className="flex h-50 w-50 text-3xl font-bold lg:text-4xl items-center mb-3">
+    <main className='flex flex-col w-[23em] md:w-[36em] bg-white rounded-lg p-7 mt-[15em] md:mt-[10em]  h-fit'>
+      <div className="flex h-50 w-50 text-3xl font-bold md:text-4xl items-center mb-3">
         <img src="/images/icon-star.svg" className='w-6 h-6 mr-3' alt="star icon"/>
         <div>FAQs</div>
       </div>
-      {prop.map((e, ind)=>(
+      {prop.map((e)=>{        
+        return (
         <>
-          {ind!=0 &&  <div className='text-slate-200'><hr/></div>}
-          <AccordionItem {...e}/>
+          {e.id!=0 &&  <div className='text-slate-200'><hr/></div>}
+          <AccordionItem {...e} setOpenItem={setOpenItem} openItem={openItem}/> 
+          
         </>
-      ))}
+      )})}
     </main>
   )
 }
